fa23cbe95def5ae60ff711a91f8af63f239e6a4d
FranciscoGomes20 <henriquecgomes10@gmail.com>
2024-05-21 16:15:05 -0500

    The get and post methods were added to the rest api

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
 
 # Create your views here.
+class CursoAPIView(APIView):
+    def get(self, reguest):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+    
+    def post(self, reguest):
+        serializer = CursoSerializer(data=reguest.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class AvaliacaoAPIView(APIView):
+    def get(self, reguest):
+        avaliacoes = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serializer.data)
+
+    def post(self, reguest):
+        serializer = AvaliacaoSerializer(data=reguest.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
